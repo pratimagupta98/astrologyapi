802aa376a36734e528a28d9c2bc15b7f1a0163ab
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-08-02 16:28:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+//const cors = require("cors");
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//require
+const users = require("./routes/users");
+ 
+ 
+//use
+app.use("/", users);
+ 
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!!");
+});
+
+//console.log(process.env.DATABASE);
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB CONNECTED SUCCEFULLY");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Example app listening on port 8000");
+});
+
+//    http://localhost:8000/admin
---
+++ controller/users.js
@@ -0,0 +1,92 @@
+const User = require("../models/users");
+
+
+// exports.signupsendotp = async (req, res) => {
+//     //       let length = 6;
+//     //     //   let otp = (
+//     //     //     "0".repeat(length) + Math.floor(Math.random() * 10 ** length)
+//     //     //   ).slice(-length);
+//     //     let otp = "123456";
+      
+//     //     const newUser = new User({
+//     //       mobile: mobile,
+//     //     });
+//     //     const findexist = await User.findOne({ mobile :mobile });
+//     //     if (findexist) {
+//     //       res.json({
+//     //         status: "success",
+//     //         msg: "Welcome Back Otp send successfully",
+//     //         registered: findexist?.mobile,
+//     //         _id: findexist?._id,
+//     //         otp: otp,
+//     //       });
+//     //     } else {
+//     //         newUser.otp = otp;
+//     //         newUser
+//     //         .save()
+//     //         .then((data) =>
+//     //           res.json({
+//     //             status: "success",
+//     //             msg: "Otp send successfully",
+//     //             registered: data?.mobile,
+//     //             _id: data?._id,
+//     //             otp: otp,
+//     //           })
+//     //         )
+//     //         .catch((error) => {
+//     //             res.status(400).json({
+//     //               status: false,
+//     //               msg: "error",
+//     //               error: error,
+//     //             });
+//     //           });
+//     //     }
+//     //   };
+    
+// }
+
+    
+    exports.signupsendotp = async (req, res) => {
+            let length = 6;
+          //   let otp = (
+          //     "0".repeat(length) + Math.floor(Math.random() * 10 ** length)
+          //   ).slice(-length);
+          let otp = "123456";
+        
+          const newUser = new User({
+            mobile: req.body.mobile,
+          });
+          const findexist = await User.findOne({ mobile :req.body.mobile });
+          if (findexist) {
+            res.json({
+              status: "success",
+              msg: "Welcome Back Otp send successfully",
+              registered: findexist?.mobile,
+              _id: findexist?._id,
+              otp: otp,
+            });
+          } else {
+              newUser.otp = otp;
+              newUser
+              .save()
+              .then((data) =>
+                res.json({
+                  status: "success",
+                  msg: "Otp send successfully",
+                  registered: data?.mobile,
+                  _id: data?._id,
+                  otp: otp,
+                })
+              )
+              .catch((error) => {
+                  res.status(400).json({
+                    status: false,
+                    msg: "error",
+                    error: error,
+                  });
+                });
+          }
+        };
+      
+  
+        
