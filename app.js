3cba651cd0ae81051cf6b241c4c7cc0f7e0da4f0
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-08-06 11:45:59 +0530

    commit

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(cors());
 
 //require
  
-//const users = require("./routes/users")
-//const astrologer = require("./routes/astrologer")
-//const admin = require("./routes/admin")
+const users = require("./routes/users")
+const astrologer = require("./routes/astrologer")
+const admin = require("./routes/admin")
 const aboutus = require("./routes/aboutus")
 const terms_condition = require("./routes/terms_condition")
 const contact_us = require("./routes/contact_us")
@@ -23,10 +23,10 @@ const contact_us = require("./routes/contact_us")
 
  
 //use
-//app.use("/", users);
-//app.use("/", astrologer);
+app.use("/", users);
+app.use("/", astrologer);
 
-//app.use("/", admin);
+app.use("/", admin);
 app.use("/", aboutus);
 app.use("/", terms_condition);
 app.use("/", contact_us);
