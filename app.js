2a7e90058ff40aec989b591f530b550047fe66fa
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-08-06 11:42:08 +0530

    commit

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+  
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Example app listening on port 8000");
--- routes/aboutus.js
+++ routes/aboutus.js
@@ -12,7 +12,7 @@ const {
 
  
  
- router.post("/admin/add_aboutus", add_aboutus);
+router.post("/admin/add_aboutus", add_aboutus);
 router.get("/admin/getAbout_us", getAbout_us);
 router.get("/admin/getone_aboutus/:id",     getone_aboutus)
 router.post("/admin/edit_aboutus/:id",     edit_aboutus);
